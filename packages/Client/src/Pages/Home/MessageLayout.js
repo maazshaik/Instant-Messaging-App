e375c9cc0d8d5ce5d2081affb0129b3e7f412b35
Abhinav <abve9575@colorado.edu>
2022-04-13 02:31:23 -0600

    More expreriments

--- packages/Client/src/Pages/Home/MessageLayout.js
+++ packages/Client/src/Pages/Home/MessageLayout.js
@@ -7,10 +7,18 @@ let textInput = React.createRef();
 axios.defaults.withCredentials = true;
 
 export class MessageLayout extends React.Component {
+  state = {
+    messages: []
+  };
+
   render() {
+    console.log(this.state.messages)
     let list = <div className="empty-message-list">No messages to display</div>;
     if (this.props.friend && this.props.friend.messages) {
       list = this.props.friend.messages.map(getMessageDetails);
+    }
+    else if (this.state.messages.length) {
+      list = this.state.messages.map(getMessageDetails);
     }
     return (
       <div className="message-layout">
@@ -18,10 +26,49 @@ export class MessageLayout extends React.Component {
 
         <div className="text-box">
           <input id="text" ref={textInput} placeholder="Type a message..." />
-          <button onClick={handleClick}>Send</button>
+          <button onClick={this.handleClick}>Send</button>
         </div>
       </div>
     );
+  }
+
+  handleClick = () => {
+    const options = {
+      method: 'GET',
+      url: 'http://localhost:3001/send',
+      params: {text: textInput.current.value, receiver: 'abhinav'},
+    }
+    axios.request(options).then((response) => {
+      console.log(response.data)
+      alert(response.data)
+      document.getElementById("text").value = ''
+    }).catch((error) => {
+      console.error(error)
+      alert("Bad")
+    })
+    this.reloadMessages()
+  }
+
+  reloadMessages = () => {
+    const options = {
+      method: 'GET',
+      url: 'http://localhost:3001/getMessages',
+      params: {target: 'abhinav'},
+    }
+  
+    axios.request(options).then((response) => {
+      var sortedMessages = response.data.sort((function (a, b) { 
+        return new Date(a.timestamp) - new Date(b.timestamp) 
+      }));
+      var newMessages = sortedMessages.map(newMessage => ({sender: newMessage.user_details, text: newMessage.message, id: 'random'}))
+      this.setState({messages: newMessages})
+      console.log('new messages')
+      this.forceUpdate()
+      //document.getElementById("text").value = ''
+    }).catch((error) => {
+      console.error(error)
+      alert("Bad")
+    })
   }
 }
 
@@ -33,38 +80,4 @@ function getMessageDetails(message) {
       text={message.text}
     ></Message>
   );
-}
-
-/*function handleClick() {
-  const options = {
-    method: 'GET',
-    url: 'http://localhost:3001/send',
-    params: {text: textInput.current.value, receiver: 'abhinav'},
-  }
-
-  axios.request(options).then((response) => {
-    console.log(response.data)
-    alert(response.data)
-    document.getElementById("text").value = ''
-  }).catch((error) => {
-    console.error(error)
-    alert("Bad")
-  })
-}*/
-
-function handleClick() {
-  const options = {
-    method: 'GET',
-    url: 'http://localhost:3001/getMessages',
-    params: {target: 'abhinav'},
-  }
-
-  axios.request(options).then((response) => {
-    console.log(response.data)
-    document.getElementById("text").value = JSON.stringify(response.data)
-    //document.getElementById("text").value = ''
-  }).catch((error) => {
-    console.error(error)
-    alert("Bad")
-  })
 }
