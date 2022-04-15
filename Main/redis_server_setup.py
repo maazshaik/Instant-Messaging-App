9ef109837e4aac714aae7aa8b98b637e7c302712
Abhinav <abve9575@colorado.edu>
2022-04-15 13:13:03 -0600

    Remove Main folder

--- Main/redis_server_setup.py
+++
@@ -1,6 +0,0 @@
-import redis
-
-redis_connection = redis.Redis(host='global-darling-cub-32426.upstash.io', port='32426', password='a2d857fccfc94c7f97a0987654eb81ee')
-
-redis_connection.set('total_users', 0)
-print(redis_connection.get('total_users').decode('utf-8'))
