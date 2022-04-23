57546ac3ba59e9b312e977aaac66c36450fd5d2d
Shaily Preetham Kurra <shaily@MacBook-Pro.local>
2022-04-23 14:45:57 -0600

    Analyzer functionality

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -1,42 +1,46 @@
-from socket import MsgFlag
-import sys
-import os
 import pika
+import redis, json
+
+redis_connection = redis.Redis(host='localhost', port=6379, db=0)
+
+def send_message(room_id, message, timestamp, userid, sender, receiver):
+    content = {
+        "user_details": userid,
+        "message": message,
+        "timestamp": timestamp,
+        "room_details": room_id,
+        "sender": sender,
+        "receiver": receiver
+    }
+    payload = json.dumps(content)
+    time = int(timestamp)
+    redis_connection.zadd(room_id, {payload: time})
+
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    def callback(ch, method, properties, body):
+        body = json.loads(body)
+        # Receive the message and flag for any inappropriate content
+        message = body['message']
+        bad_words = {"stupid", "wack", "dumb"}
+        words_list = message.split(" ")
+        print(words_list)
+        for word in words_list:
+            if word in bad_words:
+                message = "This message is flagged due to a cuss word"
+                break
+        send_message(body['room_details'], message, body['timestamp'], body['user_details'], body['sender'], body['receiver'])
+        connection.close()
 
 
-rabbitMQHost = os.getenv("RABBITMQ_HOST") or "localhost"
-print("Using host", rabbitMQHost)
 
-rabbitMQ = pika.BlockingConnection(
-        pika.ConnectionParameters(host=rabbitMQHost))
-rabbitMQChannel = rabbitMQ.channel()
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
-rabbitMQChannel.exchange_declare(exchange='logs', exchange_type='topic')
-result = rabbitMQChannel.queue_declare('', exclusive=True)
-queue_name = result.method.queue
+    channel.start_consuming()
 
-binding_keys = sys.argv[1:]
-if not binding_keys:
-    #
-    # Wildcard key will listen to anything
-    #
-    binding_keys = [ "#"]
-
-for key in binding_keys:
-    rabbitMQChannel.queue_bind(
-            exchange='logs', 
-            queue=queue_name,
-            routing_key=key)
-
-def callback(ch, method, properties, body):
-    pass
-    if contains:
-        MsgFlag
-    else:
-        "THis is a Flagged message"
-    #Redis call
-
-rabbitMQChannel.basic_consume(
-    queue=queue_name, on_message_callback=callback, auto_ack=True)
-
-rabbitMQChannel.start_consuming()
+if __name__ == '__main__':
+    main()
